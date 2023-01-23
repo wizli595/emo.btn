f4bd55f97b25880c54e0f5166e8ae2165b59c4bf
wizli595 <97339172+wizli595@users.noreply.github.com>
2023-01-23 12:32:36 +0100

    Update button.js

--- JS/button.js
+++ JS/button.js
@@ -1,8 +1,8 @@
-var colors = ["red", "green", "blue", "black"];
+var colors = ["white", "red", "green", "blue", "yellow", "orange", "black"];
 var currentColorIndex = 0;
 let body = document.querySelector("body");
 let store = localStorage.getItem("value");
-
+let emo = document.querySelector("h1");
 function changeBackground() {
   let currentColor = colors[currentColorIndex];
 
@@ -10,6 +10,35 @@ function changeBackground() {
 
   currentColorIndex = (currentColorIndex + 1) % colors.length;
   localStorage.setItem("value", (body.style.backgroundColor = currentColor));
+   if (body.style.background == "red") {
+    emo.setAttribute("class", "animate__animated animate__fadeInUpBig");
+    emo.innerHTML = "angry";
+  } else if (body.style.background == "white") {
+    emo.style.color = "black";
+    emo.setAttribute("class", "animate__animated animate__fadeInUpBig");
+    emo.innerHTML = "clean";
+  } else if (body.style.background == "green") {
+    emo.style.color = "white";
+    emo.setAttribute("class", "animate__animated animate__fadeInUpBig");
+    emo.innerHTML = "Freshness";
+  } else if (body.style.background == "blue") {
+    emo.style.color = "white";
+    emo.setAttribute("class", "animate__animated animate__fadeInUpBig");
+    emo.innerHTML = "peace";
+  } else if (body.style.background == "yellow") {
+    emo.style.color = "white";
+    emo.setAttribute("class", "animate__animated animate__fadeInUpBig");
+    emo.innerHTML = "warmth";
+  } else if (body.style.background == "black") {
+    emo.style.color = "white";
+    emo.setAttribute("class", "animate__animated animate__fadeInUpBig");
+    emo.innerHTML = "devastated";
+  } else if (body.style.background == "orange") {
+    emo.style.color = "white";
+    emo.setAttribute("class", "animate__animated animate__fadeInUpBig");
+    emo.innerHTML = "confidence";
+  }
+  
 }
 if (localStorage.value) {
   body.style.background = store;
